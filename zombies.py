e925d1ba30c8288b0a425b3e2e9853801f6c84c2
Esmeralda C. Lopez <esmeraldarekz99@gmail.com>
2016-04-10 16:08:03 -0700

    done

--- zombies.py
+++ zombies.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #Enemies in the game
 
 #HP OF ZOMBIE
@@ -7,27 +6,38 @@ class Zombie(object):
         self.health = health
         self.attack = attack 
         
-    # ATTACKS    
-=======
-#MAIN BOSS 
-class Zombie(object):
-    def __init__(self, health = 5000, attack = 2000):
-        self.health = health
-        self.attack = attack
-         
+
     #COMMAND TO ATTACK     
->>>>>>> origin/master
     def attacks(self, target):
         target.damage(self.attack)
         if target.health <= 0:
             return "Enemy down!"
         else:
             return target.health
-         #DAMAGE TAKEN   
+            
+    #DAMAGE TAKEN   
     def damage(self, amount):
         self.health -= amount
 
         
+#HP OF INFECTED
+class Infected(object):
+    def __init__(self, health = 5000, attack = 500):
+        self.health  = health 
+        self.attack = attack 
+        
+    #COMMAND ATTACK 
+    def attacks(self, target):
+        target.damage(self. attack)
+        if target.health <= 0:
+            return "Enemy down!"
+        else:
+            return target.health
+            
+    #DAMAGE TAKEN         
+    def damage(self, amount):
+        self.health -= amount 
+    
 
         
 zombie = Zombie()
