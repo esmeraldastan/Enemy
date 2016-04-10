1e79dd2dbac3e521673afc4e9a39e5d35dfd685a
Esmeralda C. Lopez <esmeraldarekz99@gmail.com>
2016-04-10 15:58:55 -0700

    done

--- zombies.py
+++ zombies.py
@@ -1,8 +1,12 @@
+#Enemies in the game
+
+#HP OF ZOMBIE
 class Zombie(object):
     def __init__(self, health = 5000, attack = 2000):
         self.health = health
         self.attack = attack 
         
+    # ATTACKS    
     def attacks(self, target):
         target.damage(self.attack)
         if target.health <= 0:
@@ -12,8 +16,9 @@ class Zombie(object):
             
     def damage(self, amount):
         self.health -= amount
+
         
-          
+#HP OF INFECTED        
 class Infected(object):
     def __init__(self, health = 5000, attack = 500):
         self.health  = health 
