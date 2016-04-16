64baafa89fc2dc7e6742949499a9ea144422232b
Esmeralda C. Lopez <esmeraldarekz99@gmail.com>
2016-04-15 23:07:26 -0700

    4/15

--- zombies.py
+++ zombies.py
@@ -1,8 +1,12 @@
 #Enemies in the game
+#command = raw_input('>')
+
+#print "attack"
+
 
 #HP OF ZOMBIE
 class Zombie(object):
-    def __init__(self, health = 5000, attack = 2000):
+    def __init__(self, health = 5000, attack = 15000):
         self.health = health
         self.attack = attack 
         
@@ -18,8 +22,34 @@ class Zombie(object):
     #DAMAGE TAKEN   
     def damage(self, amount):
         self.health -= amount
-
         
+zombie = Zombie()
+
+
+#player status 
+class player(object):
+    
+    def __init__(self, name, health = 50000, attack = 500):
+        self.name = name 
+        self.health = health 
+        
+class single_player(player):
+    def __init__(self, name, health = 49800):
+        super(single_player, self).__init__(name, health = 49800)
+        
+        
+    def attacks(self, target):
+        target.damage(self.attack)
+        if target.health <= 0:
+            return "Enemy down!"
+        else:
+            return target.health
+        
+me = single_player(player)        
+
+#if command == "me.attacks         
+                
+                                
 #HP OF INFECTED
 class Infected(object):
     def __init__(self, health = 5000, attack = 500):
